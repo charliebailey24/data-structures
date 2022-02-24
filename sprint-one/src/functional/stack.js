58be53d2c1c264fd39255453d4975af8a8f10900
Charlie <charliebailey24@gmail.com>
2022-02-24 12:40:44 -0800

    Initial work to understand the problem space and the i/o. starting on pseudo

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function() {
+  // I think this is the maker function; it is capitalized
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,6 +7,16 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // notes: unsure what the relationship is between storage and someInstance.
+
+    // input: some value
+    // output: value added to someInstance (or storage?) at the "top of the stack"
+
+    //
+
+
+
+
   };
 
   someInstance.pop = function() {
