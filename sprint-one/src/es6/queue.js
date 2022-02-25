ae2c3af2673f851e254f78036320899a821634a4
Charlie <charliebailey24@gmail.com>
2022-02-25 12:29:08 -0800

    Implemented ES6 style

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.tail = 0;
+  }
+
+  enqueue(value) {
+    this.tail += 1;
+    this.storage[this.tail] = value;
+  }
+
+  dequeue() {
+    var keys = Object.keys(this.storage);
+    var min = Math.min(...keys);
+
+    var headValue = this.storage[min];
+    delete this.storage[min];
+    return headValue;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
+
+var NewQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.top = 0;
+  }
+
+  push(value) {
+    this.top += 1;
+    this.storage[this.top] = value;
+  }
+
+  pop() {
+    var topValue = this.storage[this.top];
+    delete this.storage[this.top];
+    if (this.top > 0) {
+      this.top -= 1;
+    }
+    return topValue;
+  }
+
+  size() {
+    return this.top;
   }
 
 }
+
+var NewStack = new Stack();
