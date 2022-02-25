783b2b9ff45176d1374d8154e2c6faf710a51b80
Charlie <charliebailey24@gmail.com>
2022-02-25 11:07:18 -0800

    Implemented the function-shared pattern for queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -5,10 +5,36 @@ var Queue = function() {
   someInstance.storage = {};
   someInstance.tail = 0;
   _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+
+  for (var key in this.storage) {
+    key = Number(key);
+    if (key > this.tail) {
+      this.tail = key;
+    }
+  }
+  var nextKey = this.tail + 1;
+  this.storage[nextKey] = value;
+};
+
+queueMethods.dequeue = function() {
+  var keys = Object.keys(this.storage);
+  var min = Math.min(...keys);
+
+  var headValue = this.storage[min];
+  delete this.storage[min];
+
+  return headValue;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
 
