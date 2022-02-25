42c0b0d5fafae243732989cb82d8752d83e87fea
Charlie <charliebailey24@gmail.com>
2022-02-24 17:27:36 -0800

    Implemented functional instantiation of queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,42 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var tail = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+
+    // input: value
+    // output: value added to the next avaliable index in storage
+
+    for (var key in storage) {
+      key = Number(key);
+      if (key > tail) {
+        tail = key;
+      }
+    }
+
+    var nextKey = tail + 1;
+
+    storage[nextKey] = value;
   };
 
   someInstance.dequeue = function() {
+
+    // return the value in storage with the smallest value
+    var keys = Object.keys(storage);
+    var min = Math.min(...keys);
+
+    var headValue = storage[min];
+    delete storage[min];
+
+    return headValue;
   };
 
   someInstance.size = function() {
+
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,28 +1,50 @@
 var Stack = function() {
-  // I think this is the maker function; it is capitalized
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var top = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    // notes: unsure what the relationship is between storage and someInstance.
 
     // input: some value
     // output: value added to someInstance (or storage?) at the "top of the stack"
 
-    //
+    // get max key in storage
+    // add value at max key
 
+    for (var key in storage) {
+      key = Number(key);
+      if (key > top) {
+        top = key;
+      }
+    }
+    var nextKey = top + 1;
 
-
-
+    storage[nextKey] = value;
   };
 
   someInstance.pop = function() {
+    for (var key in storage) {
+      key = Number(key);
+      if (key > top) {
+        top = key;
+      }
+    }
+
+    var topValue = storage[top];
+    delete storage[top];
+    if (top > 0) {
+      top -= 1;
+    }
+
+    return topValue;
   };
 
   someInstance.size = function() {
+
+    return Object.keys(storage).length;
   };
 
   return someInstance;
