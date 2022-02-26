fc85a3f717ffadfd962b0b7b6253ff67bf23fbe5
Charlie <charliebailey24@gmail.com>
2022-02-25 17:01:06 -0800

    Implemented addToTail and removeFromHead in LL. Close to getting contains working.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,59 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // input: string value
+    // output: the value added to the apporpriate node, added to the tail
+
+    // question: what is the list actually going to look like?
+    // I'm confused about the relationship between each node and the list object
+    // I obviously need something to hold the nodes... or do I?
+
+    // list = {head: {node}, ... tail: {node}}
+
+    // create a new node
+    // add node to list
+    // if the current tail is null
+      // add the new node to list
+      // update the list tail reference to the new node (how do I store the new node in list?)
+    // otherwise
+      // adjust the current tails next reference to the new node
+
+    var NewNode = Node(value);
+    list.value = NewNode;
+    // should I check if head is null here too?
+    if (list.tail === null) {
+      // set tail pointer to new node... how do I do this?
+      list.tail = list.value;
+    }
+    if (list.tail !== null) {
+      // adjust current tail's next reference to refer to NewNode
+      list.tail.next = list.value;
+      // change tail reference itself to refer to new last value
+      list.tail = list.value;
+    }
+    if (list.head === null) {
+      list.head = list.value;
+    }
   };
 
   list.removeHead = function() {
+    // move head reference to next item in list AFTER ORIGINAL HEAD
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    if (node === undefined) {
+      node = list.head;
+    }
+    if (node.value === target) {
+      return true;
+    }
+    if (node.value === null) {
+      return false;
+    }
+    return list.contains(target, node.next);
   };
 
   return list;
@@ -19,7 +66,8 @@ var Node = function(value) {
   var node = {};
 
   node.value = value;
-  node.next = null;
+  // need to deal with this... how do I get the next node?
+  node.next = null; // terminal
 
   return node;
 };
